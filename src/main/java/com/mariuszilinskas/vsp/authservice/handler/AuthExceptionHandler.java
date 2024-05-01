c995836840ebb0c03d8965b87c640b5bc61ba6a8
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-01 01:01:37 +0100

    created exception handler and dto objects

---
+++ src/main/java/com/mariuszilinskas/vsp/authservice/handler/AuthExceptionHandler.java
@@ -0,0 +1,87 @@
+package com.mariuszilinskas.vsp.authservice.handler;
+
+
+import com.mariuszilinskas.vsp.authservice.dto.ErrorResponse;
+import com.mariuszilinskas.vsp.authservice.dto.FieldErrorResponse;
+import com.mariuszilinskas.vsp.authservice.exception.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class is a global exception handler that handles exceptions thrown across the auth service.
+ *
+ * @author Marius Zilinskas
+ */
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthExceptionHandler.class);
+
+    /**
+     * Exception for Request Validations
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<FieldErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        FieldErrorResponse errorResponse = new FieldErrorResponse(errors, status.value(), status.getReasonPhrase());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    // --------------------- General ------------------------------
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // --------------------- Specific -----------------------------
+
+    @ExceptionHandler(PasscodeValidationException.class)
+    public ResponseEntity<ErrorResponse> handlePinCodeDoesntMatchException(PasscodeValidationException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PasswordValidationException.class)
+    public ResponseEntity<ErrorResponse> handlePasswordValidationException(PasswordValidationException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResetTokenValidationException.class)
+    public ResponseEntity<ErrorResponse> handleResetTokenValidationException(ResetTokenValidationException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // -----------------------------------------------------------
+
+    /**
+     * This method builds the error response for a given exception.
+     *
+     * @param message the exception message
+     * @param status the HTTP status
+     * @return a ResponseEntity that includes the error response and the given HTTP status
+     */
+    private ResponseEntity<ErrorResponse> buildErrorResponse(String message, HttpStatus status) {
+        logger.error("Status: {}, Message: '{}'", status.value(), message);
+        ErrorResponse errorResponse = new ErrorResponse(message, status.value(), status.getReasonPhrase());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}
---
+++ src/main/java/com/mariuszilinskas/vsp/authservice/util/AuthUtils.java
@@ -0,0 +1,12 @@
+package com.mariuszilinskas.vsp.authservice.util;
+
+public abstract class AuthUtils {
+
+    private AuthUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+    public static final long PIN_CODE_VALID_MINUTES = 15;
+}
