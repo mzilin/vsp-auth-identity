4bdb9b773f15e94451a248e25b2b7e5d3d5bab46
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-02 22:32:51 +0100

    added tests for verifyPasscode

--- src/test/java/com/mariuszilinskas/vsp/authservice/service/AuthServiceImplTest.java
+++ src/test/java/com/mariuszilinskas/vsp/authservice/service/AuthServiceImplTest.java
@@ -1,6 +1,11 @@
 package com.mariuszilinskas.vsp.authservice.service;
 
+import com.mariuszilinskas.vsp.authservice.client.UserFeignClient;
 import com.mariuszilinskas.vsp.authservice.dto.CreateCredentialsRequest;
+import com.mariuszilinskas.vsp.authservice.dto.VerifyPasscodeRequest;
+import com.mariuszilinskas.vsp.authservice.exception.EmailVerificationException;
+import com.mariuszilinskas.vsp.authservice.exception.PasscodeExpiredException;
+import com.mariuszilinskas.vsp.authservice.exception.PasscodeValidationException;
 import com.mariuszilinskas.vsp.authservice.model.HashedPassword;
 import com.mariuszilinskas.vsp.authservice.model.Passcode;
 import com.mariuszilinskas.vsp.authservice.repository.HashedPasswordRepository;
@@ -17,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.UUID;
@@ -28,16 +34,19 @@ import static org.mockito.Mockito.*;
 public class AuthServiceImplTest {
 
     @Mock
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Mock
-    private HashedPasswordRepository hashedPasswordRepository;
-
-    @Mock
     private TokenGenerationService tokenGenerationService;
 
     @Mock
     private PasscodeRepository passcodeRepository;
+
+    @Mock
+    private UserFeignClient userFeignClient;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Mock
+    private HashedPasswordRepository hashedPasswordRepository;
 
     @InjectMocks
     AuthServiceImpl authService;
@@ -53,6 +62,7 @@ public class AuthServiceImplTest {
     void setUp() {
         hashedPassword.setPasswordHash("encodedPassword");
         passcode.setPasscode("123456");
+        passcode.setExpiryDate(Instant.now().plusSeconds(10));
 
         // Empty string for URL as a placeholder
         Request feignRequest = Request.create(
@@ -71,7 +81,7 @@ public class AuthServiceImplTest {
     // ------------------------------------
 
     @Test
-    void testCreatePasswordAndSetPasscode_newUserCredentials() {
+    void testCreatePasswordAndSetPasscode_NewUserCredentials() {
         // Arrange
         CreateCredentialsRequest request = new CreateCredentialsRequest(userId, "user@example.com", "password123");
 
@@ -105,7 +115,7 @@ public class AuthServiceImplTest {
     }
 
     @Test
-    void testCreatePasswordAndSetPasscode_existingUserCredentials() {
+    void testCreatePasswordAndSetPasscode_ExistingUserCredentials() {
         // Arrange
         UUID existingUserId = UUID.randomUUID();
         CreateCredentialsRequest request = new CreateCredentialsRequest(existingUserId, "user@example.com", "password123");
@@ -134,6 +144,76 @@ public class AuthServiceImplTest {
         Passcode savedPasscode = passcodeCaptor.getValue();
         assertEquals(existingUserId, savedPasscode.getUserId());
         assertEquals(passcode.getPasscode(), savedPasscode.getPasscode());
+    }
+
+    // ------------------------------------
+
+    @Test
+    void verifyPasscode_Success() {
+        // Arrange
+        VerifyPasscodeRequest request = new VerifyPasscodeRequest(passcode.getPasscode());
+
+        when(passcodeRepository.findByUserId(userId)).thenReturn(Optional.of(passcode));
+        when(userFeignClient.verifyUserEmail(userId)).thenReturn(null);
+        doNothing().when(passcodeRepository).deleteByUserId(userId);
+
+        // Act
+        authService.verifyPasscode(userId, request);
+
+        // Assert
+        verify(passcodeRepository, times(1)).findByUserId(userId);
+        verify(userFeignClient, times(1)).verifyUserEmail(userId);
+        verify(passcodeRepository, times(1)).deleteByUserId(userId);
+    }
+
+    @Test
+    void verifyPasscode_ExpiredPasscode() {
+        // Arrange
+        passcode.setExpiryDate(Instant.now().minusSeconds(10));
+        VerifyPasscodeRequest request = new VerifyPasscodeRequest(passcode.getPasscode());
+
+        when(passcodeRepository.findByUserId(userId)).thenReturn(Optional.of(passcode));
+
+        // Act & Assert
+        assertThrows(PasscodeExpiredException.class, () -> authService.verifyPasscode(userId, request));
+
+        // Assert
+        verify(passcodeRepository, times(1)).findByUserId(userId);
+        verify(userFeignClient, never()).verifyUserEmail(userId);
+        verify(passcodeRepository, never()).deleteByUserId(userId);
+    }
+
+    @Test
+    void verifyPasscode_IncorrectPasscode() {
+        // Arrange
+        VerifyPasscodeRequest request = new VerifyPasscodeRequest("wrong1");
+
+        when(passcodeRepository.findByUserId(userId)).thenReturn(Optional.of(passcode));
+
+        // Act & Assert
+        assertThrows(PasscodeValidationException.class, () -> authService.verifyPasscode(userId, request));
+
+        // Assert
+        verify(passcodeRepository, times(1)).findByUserId(userId);
+        verify(userFeignClient, never()).verifyUserEmail(userId);
+        verify(passcodeRepository, never()).deleteByUserId(userId);
+    }
+
+    @Test
+    void verifyPasscode_FailsToVerifyEmail() {
+        // Arrange
+        VerifyPasscodeRequest request = new VerifyPasscodeRequest(passcode.getPasscode());
+
+        when(passcodeRepository.findByUserId(userId)).thenReturn(Optional.of(passcode));
+        doThrow(feignException).when(userFeignClient).verifyUserEmail(any(UUID.class));
+
+        // Act & Assert
+        assertThrows(EmailVerificationException.class, () -> authService.verifyPasscode(userId, request));
+
+        // Assert
+        verify(passcodeRepository, times(1)).findByUserId(userId);
+        verify(userFeignClient, times(1)).verifyUserEmail(userId);
+        verify(passcodeRepository, never()).deleteByUserId(userId);
     }
 
     // ------------------------------------
