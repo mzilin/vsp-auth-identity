95366324b389ce8fce47eb1797b06f9b39e95f28
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-08 16:24:38 +0100

    update user feign client

--- src/main/java/com/mariuszilinskas/vsp/authservice/client/UserFeignClient.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/client/UserFeignClient.java
@@ -1,6 +1,8 @@
 package com.mariuszilinskas.vsp.authservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,7 +11,9 @@ import java.util.UUID;
 @FeignClient("users")
 public interface UserFeignClient {
 
-    @PatchMapping(value = "/{userId}/email/verify", consumes = "application/json")
-    void verifyUserEmail(@PathVariable("userId") UUID userId);
+    @PatchMapping(value = "/{userId}/verify-email", consumes = "application/json")
+    ResponseEntity<Void> verifyUserEmail(@PathVariable("userId") UUID userId);
 
+    @GetMapping(value = "/id-by-email", consumes = "application/json")
+    UUID getUserIdByEmail(String email);
 }
