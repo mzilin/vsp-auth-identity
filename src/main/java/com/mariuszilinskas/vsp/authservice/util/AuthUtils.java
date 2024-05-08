b15a2982a2dbeb0ef8fbbacff1f5b12e0c3f2dce
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-08 16:24:13 +0100

    changed PIN_CODE_VALID_MILLIS name to FIFTEEN_MINS_IN_MILLIS

--- src/main/java/com/mariuszilinskas/vsp/authservice/util/AuthUtils.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/util/AuthUtils.java
@@ -8,6 +8,6 @@ public abstract class AuthUtils {
 
     public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
 
-    public static final long PIN_CODE_VALID_MILLIS = 15 * 60 * 1000; // 15 minutes
+    public static final long FIFTEEN_MINS_IN_MILLIS = 15 * 60 * 1000; // 15 minutes
 
 }
