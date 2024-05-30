b53732d4a91735d640776e43a0483d2420b5ea75
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-31 00:43:20 +0100

    added email query param to id-by-email feign endpoint

--- src/main/java/com/mariuszilinskas/vsp/authservice/client/UserFeignClient.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/client/UserFeignClient.java
@@ -3,9 +3,7 @@ package com.mariuszilinskas.vsp.authservice.client;
 import com.mariuszilinskas.vsp.authservice.enums.UserRole;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -15,8 +13,8 @@ public interface UserFeignClient {
     @PatchMapping(value = "/{userId}/verify", consumes = "application/json")
     ResponseEntity<Void> verifyUserEmail(@PathVariable("userId") UUID userId);
 
-    @GetMapping(value = "/id-by-email", consumes = "application/json")
-    UUID getUserIdByEmail(String email);
+    @GetMapping(value = "/data/id-by-email", consumes = "application/json")
+    UUID getUserIdByEmail(@RequestParam String email);
 
     @GetMapping(value = "/{userId}/role", consumes = "application/json")
     UserRole getUserRole(@PathVariable("userId") UUID userId);
