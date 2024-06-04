ba432e5d5bc3ebcb961c258afe56fc2a744184f3
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-04 02:55:54 +0100

    updated AuthUtils

--- src/main/java/com/mariuszilinskas/vsp/authservice/util/AuthUtils.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/util/AuthUtils.java
@@ -6,8 +6,18 @@ public abstract class AuthUtils {
         // Private constructor to prevent instantiation
     }
 
+    public static final String PRODUCTION_ENV = "production";
+
     public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
 
-    public static final long FIFTEEN_MINS_IN_MILLIS = 15 * 60 * 1000; // 15 minutes
+    public static final String ACCESS_TOKEN_NAME = "vsp_access";
+
+    public static final String REFRESH_TOKEN_NAME = "vsp_refresh";
+
+    public static final long FIFTEEN_MINUTES_IN_MILLIS = 15 * 60 * 1000L; // 15 minutes
+
+    public static final long ACCESS_TOKEN_EXPIRATION_MILLIS = FIFTEEN_MINUTES_IN_MILLIS;
+
+    public static final long REFRESH_TOKEN_EXPIRATION_MILLIS = 7 * 24 * 60 * 60 * 1000L; // 7 days
 
 }
