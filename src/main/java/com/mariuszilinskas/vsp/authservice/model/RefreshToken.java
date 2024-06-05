3e35bbf82d9ff8bba395b96d49e8d924540b2fc1
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-05 02:26:33 +0100

    removed generation strategy from refresh token model

--- src/main/java/com/mariuszilinskas/vsp/authservice/model/RefreshToken.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/model/RefreshToken.java
@@ -21,7 +21,6 @@ import java.util.UUID;
 public class RefreshToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "user_id", nullable = false)
