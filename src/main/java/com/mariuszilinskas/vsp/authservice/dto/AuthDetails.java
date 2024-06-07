e0935e2625afaec4779b7c06c5071b16c1050ffb
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-07 19:12:01 +0100

    update AuthDetails dto record

--- src/main/java/com/mariuszilinskas/vsp/authservice/dto/AuthDetails.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/dto/AuthDetails.java
@@ -1,10 +1,15 @@
 package com.mariuszilinskas.vsp.authservice.dto;
 
+import com.mariuszilinskas.vsp.authservice.enums.UserAuthority;
+import com.mariuszilinskas.vsp.authservice.enums.UserRole;
+import com.mariuszilinskas.vsp.authservice.enums.UserStatus;
+
 import java.util.List;
 import java.util.UUID;
 
 public record AuthDetails(
         UUID userId,
-        List<String> roles,
-        List<String> authorities
+        List<UserRole> roles,
+        List<UserAuthority> authorities,
+        UserStatus status
 ) {}
