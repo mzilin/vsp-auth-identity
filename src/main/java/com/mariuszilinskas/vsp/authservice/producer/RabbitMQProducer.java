1763fa6b657d69e51c8bc9c34e82a7e43b4141ac
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-21 23:28:39 +0100

    rename EmailMessageProducer to RabbitMQProducer

--- src/main/java/com/mariuszilinskas/vsp/authservice/producer/RabbitMQProducer.java
+++ src/main/java/com/mariuszilinskas/vsp/authservice/producer/RabbitMQProducer.java
@@ -11,25 +11,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class EmailMessageProducer {
+public class RabbitMQProducer {
 
-    private static final Logger logger = LoggerFactory.getLogger(EmailMessageProducer.class);
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProducer.class);
     private final RabbitTemplate rabbitTemplate;
 
     @Value("${rabbitmq.exchange}")
     private String exchange;
 
     @Value("${rabbitmq.routing-keys.platform-emails}")
-    private String routingKey;
+    private String platformEmailsRoutingKey;
 
     public void sendVerificationEmailMessage(VerifyEmailRequest request) {
         logger.info("Sending Verification Email message: {}", request);
-        rabbitTemplate.convertAndSend(exchange, routingKey, request);
+        rabbitTemplate.convertAndSend(exchange, platformEmailsRoutingKey, request);
     }
 
     public void sendWelcomeEmailMessage(WelcomeEmailRequest request) {
         logger.info("Sending Welcome Email message: {}", request);
-        rabbitTemplate.convertAndSend(exchange, routingKey, request);
+        rabbitTemplate.convertAndSend(exchange, platformEmailsRoutingKey, request);
     }
 
 }
